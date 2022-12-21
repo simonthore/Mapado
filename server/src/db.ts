cc099ce7876053f8f243a2ad875bfff8d88a3cde
Simon Thore <simon.thore@hotmail.fr>
2022-12-21 16:31:10 +0100

    test config server

---
+++ server/src/db.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+import { env, loadEnv } from './env';
+
+
+loadEnv();
+
+export default new DataSource({
+    type: 'postgres',
+    host: 'db',
+    port: 5432,
+    username: env.POSTGRES_USER,
+    password: env.POSTGRES_PASSWORD,
+    database: env.POSTGRES_DB,
+    synchronize: true,
+    // entities: [Counter,WaitingRoom,User],
+    logging: ["error"],
+});
