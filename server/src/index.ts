c4d38aeb2c3a51ed85abf6e6646d777057e41efd
Simon Thore <simon.thore@hotmail.fr>
2022-12-22 10:44:57 +0100

    server repond

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,15 @@
 import express, { Express, Request, Response } from "express";
-import path from "path";
 import cors from "cors";
 
 
-
+console.log('Hello World')
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('<h1>Bienvenue sur City Guide</h1>')
+    res.send('<h1>Bienvenue sur City Simon</h1>')
 });
 
 const port = process.env.PORT ||8000;
