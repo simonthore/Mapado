5f2adcb37f5df049c68b282663fefb49ba17692b
Ikunat <francois.deleau@pm.me>
2022-12-22 11:46:20 +0100

    port 4000, gitignore server, port db 5321

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Salut c'est Simon je garde les yeux tournés vers Namek</p>
+      <p>
+        Salut c'est Simon je garde les yeux tournés vers Namek et Petit coeur
+        dsqjkdnjkqs
+      </p>
     </div>
   );
 }
--- server/src/db.ts
+++ server/src/db.ts
@@ -1,17 +1,16 @@
-import { DataSource } from 'typeorm';
-import { env, loadEnv } from './env';
-
+import { DataSource } from "typeorm";
+import { env, loadEnv } from "./env";
 
 loadEnv();
 
 export default new DataSource({
-    type: 'postgres',
-    host: 'db',
-    port: 5432,
-    username: env.POSTGRES_USER,
-    password: env.POSTGRES_PASSWORD,
-    database: env.POSTGRES_DB,
-    synchronize: true,
-    // entities: [Counter,WaitingRoom,User],
-    logging: ["error"],
+  type: "postgres",
+  host: "db",
+  port: 5321,
+  username: env.POSTGRES_USER,
+  password: env.POSTGRES_PASSWORD,
+  database: env.POSTGRES_DB,
+  synchronize: true,
+  // entities: [Counter,WaitingRoom,User],
+  logging: ["error"],
 });
