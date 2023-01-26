ef4d401e93babb896677f65b7fa0db4f3669138d
Anna Gosme <annagosme@air-de-anna.lan>
2023-01-26 10:40:05 +0100

    begin authentication work

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,32 +1,70 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
-import City from './City';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Field, InputType, ObjectType } from "type-graphql";
+import City from "./City";
+import { IsEmail, Matches, MinLength } from "class-validator";
+import { argon2id, hash, verify } from "argon2";
+import { userInfo } from "os";
 
 @Entity()
+@ObjectType()
 class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    Role_id: number;
+  @Field()
+  @Column()
+  role_id: number;
 
-    @Column()
-    City_id: number;
+  @Field()
+  @Column()
+  city_id: number;
 
-    @Column({ length: 25 })
-    Username: string;
+  @Column({ nullable: true })
+  email: string;
 
-    @Column({ length: 20 })
-    Password: string;
+  @Column({ nullable: true })
+  hasedPassword?: string;
 
-    @Column({ length: 50 })
-    Email: string;
+  @Field()
+  @Column({ enum: ["visitor", "cityAdmin", "SuperAdmin"], default: "visitor" })
+  role: Role;
 
-    @Column()
-    Rights: string;
-
-    @Column()
-    @OneToMany(() => City, (c) => c.user)
-    city: City;
+  @Column()
+  @OneToMany(() => City, (c) => c.user)
+  city: City;
 }
+
+@InputType()
+export class UserInput {
+    @Field()
+    @IsEmail()
+    email: string;
+
+    @Field()
+    @MinLength(8)
+    @Matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/)
+    password: string;
+}
+
+const hashingOptions = {
+    memoryCost: 2 ** 16,
+    timeCost: 5,
+    type: argon2id,
+}
+
+export const hashedPassword = async (plainPassword: string): Promise<string> => 
+await hash(plainPassword, hashingOptions);
+
+export const verifyPassword = async (
+    plainPassword: string,
+    hashedPassword: string
+): Promise<boolean> => 
+    await verify(hashedPassword, plainPassword, hashingOptions);
+
+export const getSafeAttributes = (use: User) => ({
+    ...userInfo,
+    hashedPassword: undefined
+});
 
 export default User;
