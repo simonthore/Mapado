890035c6aea67d74d2760fe53658d6a733af7989
Anna Gosme <annagosme@air-de-anna.lan>
2023-01-26 17:18:38 +0100

    create home page & manage city page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,65 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import CSS from 'csstype';
+import Home from "./screens/Home";
+import Header from './components/Header';
+import AddManageCities from './components/AddManageCities';
 import "./App.css";
+import bordeaux from "./assets/bordeaux.jpeg";
+import lille from "./assets/lille.jpeg";
+import paris from "./assets/paris.jpeg";
+
+
+const styles: CSS.Properties = {
+  margin: 0,
+  backgroundSize: 'cover',
+  backgroundColor: '#3270F4',
+};
+
+const cities: {
+  id: number;
+  name: string;
+  city_area: string;
+  photo?: string;
+  user: {}[];
+}[] = [
+  {
+    id: 1,
+    name: "Bordeaux",
+    city_area: "44.8473900254569, -0.7188799313856008",
+    photo: bordeaux,
+    user: ["1", "2", "3"],
+  },
+  {
+    id: 2,
+    name: "Lille",
+    city_area: "50.62976866778606, 3.04621070309212",
+    photo: lille,
+    user: ["4", "5", "6"],
+  },
+  {
+    id: 3,
+    name: "Paris",
+    city_area: "48.87170691129766, 2.325403812427441",
+    photo: paris,
+    user: ["7", "8", "9"],
+  },
+  {
+    id: 4,
+    name: "Marseille",
+    city_area: "43.332225160179654, 5.398493055776797",
+    user: ["10", "11", "12"],
+  },
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Salut c'est Simon je garde les yeux tournés vers Namek et Petit coeur
-        dsqjkdnjkqs
-      </p>
+    <div style={styles}>
+      <Header />
+      <Routes>
+      <Route path="/" element={<Home cities={cities} />} />
+      <Route path="/manage-cities" element={<AddManageCities cities={cities}/>} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,37 @@
+import CSS from "csstype";
+
+export default function Header() {
+
+    const HeaderStyles: CSS.Properties = {
+        height: '8.6rem',
+        width: '100vw',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: '#E6BFFB',
+        fontFamily: 'Rubik',
+        fontWeight: 800,
+        fontSize: '3rem',
+        color: '#000000',
+        
+    }
+
+    const searchBarStyles: CSS.Properties = {
+        height: '2.5rem',
+        width: '20rem',
+        fontWeight: 700,
+        fontSize: '1rem',
+        color: '#000000',
+        borderRadius: '10px',
+        paddingLeft: '20px',
+    }
+
+
+    return (
+        <div style={HeaderStyles}>
+            <div>MAPADO</div>
+            <input style={searchBarStyles} type="text" placeholder="Rechercher une ville"></input>
+        </div>
+    )
+}
