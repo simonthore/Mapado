6739ecdeff3ee6b281e0bc2675a4447c5fb8a875
ggors <ggorski@consoneo.com>
2023-01-27 00:23:51 +0100

    💄 Adding style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import {gql, useQuery} from '@apollo/client';
+import binoculars from "./assets/images/binculars.png"
 
 const GET_CITIES = gql`
 query Cities {
@@ -15,21 +16,55 @@ interface Cities {
     id: number;
     name: string;
 }
+
 function App() {
-    const {data} = useQuery<{cities: Cities[]}>(GET_CITIES);
-console.log(data)
+    const {data} = useQuery<{ cities: Cities[] }>(GET_CITIES);
+    console.log(data)
 
     const cities = data?.cities || []
 
+    const fakeCities = [
+        {
+            "id": 3,
+            "name": "Bordeaux"
+        },
+        {
+            "id": 4,
+            "name": "Nice"
+        },
+        {
+            "id": 5,
+            "name": "Lyon"
+        },
+        {
+            "id": 6,
+            "name": "Paris"
+        },
+        {
+            "id": 8,
+            "name": "Marseille"
+        },
+        {
+            "id": 9,
+            "name": "Nantes"
+        },
+        {
+            "id": 10,
+            "name": "Rennes"
+        }
+    ]
     return (
         <div className="App">
-            <p>
-                Salut c'est Simon je garde les yeux tournés vers Namek et Petit coeur
-                dsqjkdnjkqs
-            </p>
-            <div>
-                {cities.map((c) =>
-                    <h1>{c.name}</h1>
+            <div className={"title"}>
+                <h1>
+                    Mapado
+                </h1>
+                <img src={binoculars} alt={"binoculars"}/>
+            </div>
+
+            <div className={"cities"}>
+                {fakeCities.map((c) =>
+                    <h1 className={"city"}>{c.name}</h1>
                 )}
             </div>
         </div>
