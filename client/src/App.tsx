f3ceb1b42e6cc736b1d4b56b6bdae0134bcaea8a
ggors <ggorski@consoneo.com>
2023-01-26 23:30:24 +0100

    ✨ calling Cities from db with front

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,39 @@
 import React from "react";
 import "./App.css";
+import {gql, useQuery} from '@apollo/client';
 
+const GET_CITIES = gql`
+query Cities {
+  cities {
+    id
+    name
+  }
+}
+`
+
+interface Cities {
+    id: number;
+    name: string;
+}
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Salut c'est Simon je garde les yeux tournés vers Namek et Petit coeur
-        dsqjkdnjkqs
-      </p>
-    </div>
-  );
+    const {data} = useQuery<{cities: Cities[]}>(GET_CITIES);
+console.log(data)
+
+    const cities = data?.cities || []
+
+    return (
+        <div className="App">
+            <p>
+                Salut c'est Simon je garde les yeux tournés vers Namek et Petit coeur
+                dsqjkdnjkqs
+            </p>
+            <div>
+                {cities.map((c) =>
+                    <h1>{c.name}</h1>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
