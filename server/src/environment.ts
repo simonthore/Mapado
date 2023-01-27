4a18d2dfcd4ead28b1edd87304877e89b72d3d80
Ikunat <francois.deleau@pm.me>
2023-01-27 10:09:09 +0100

    Suppression du .env en trop

--- server/src/environment.ts
+++
@@ -1,9 +0,0 @@
-import { load } from "ts-dotenv";
-
-export const env = load({
-    JWT_PRIVATE_KEY: String,
-    CORS_ALLOWED_ORIGIN: String,
-    NODE_ENV: ['production' as const, 'development' as const],
-    SERVER_HOST: String,
-    SERVER_PORT: Number,
-});
