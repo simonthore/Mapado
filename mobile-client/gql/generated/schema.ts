245ff83c36ee64aa79bd9c6e3b64b16beab69670
ggors <ggorski@consoneo.com>
2023-01-28 14:09:56 +0100

    ⬆️ React Native config for graphql

---
+++ mobile-client/gql/generated/schema.ts
@@ -0,0 +1,96 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type City = {
+  __typename?: 'City';
+  id: Scalars['Float'];
+  name: Scalars['String'];
+};
+
+export type CityInput = {
+  name: Scalars['String'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createCity: City;
+  deleteCity: Scalars['Boolean'];
+  updateCity: City;
+};
+
+
+export type MutationCreateCityArgs = {
+  data: CityInput;
+};
+
+
+export type MutationDeleteCityArgs = {
+  id: Scalars['Int'];
+};
+
+
+export type MutationUpdateCityArgs = {
+  data: CityInput;
+  id: Scalars['Int'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  cities: Array<City>;
+};
+
+export type CitiesQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type CitiesQuery = { __typename?: 'Query', cities: Array<{ __typename?: 'City', id: number, name: string }> };
+
+
+export const CitiesDocument = gql`
+    query Cities {
+  cities {
+    id
+    name
+  }
+}
+    `;
+
+/**
+ * __useCitiesQuery__
+ *
+ * To run a query within a React component, call `useCitiesQuery` and pass it any options that fit your needs.
+ * When your component renders, `useCitiesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useCitiesQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useCitiesQuery(baseOptions?: Apollo.QueryHookOptions<CitiesQuery, CitiesQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<CitiesQuery, CitiesQueryVariables>(CitiesDocument, options);
+      }
+export function useCitiesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<CitiesQuery, CitiesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<CitiesQuery, CitiesQueryVariables>(CitiesDocument, options);
+        }
+export type CitiesQueryHookResult = ReturnType<typeof useCitiesQuery>;
+export type CitiesLazyQueryHookResult = ReturnType<typeof useCitiesLazyQuery>;
+export type CitiesQueryResult = Apollo.QueryResult<CitiesQuery, CitiesQueryVariables>;
