25d6c4e998e61d8f3e88a86a54fd5af5d45fae41
ggors <ggorski@consoneo.com>
2023-02-02 14:40:45 +0100

    ♻️ Refacto
     Refacto after rebase from main as "authentification" branch merged on main

--- server/src/resolver/UserResolver.ts
+++ server/src/resolver/UserResolver.ts
@@ -1,37 +1,39 @@
-import { ApolloError } from "apollo-server-errors";
-import { verify } from "crypto";
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import {ApolloError} from "apollo-server-errors";
+import {Arg, Mutation, Query, Resolver} from "type-graphql";
 import datasource from "../db";
-import User, { hashPassword, UserInput, verifyPassword } from "../entity/User";
+import User, {hashPassword, UserInput, verifyPassword} from "../entity/User";
 
 @Resolver(User)
 export class UserResolver {
-  @Query(() => [User])
-  async users(): Promise<User[]> {
-    return await datasource.getRepository(User).find();
-  }
-  @Mutation(() => User)
-  async createUser(@Arg("data") data: UserInput): Promise<User> {
-    const hashedPassword = await hashPassword(data.password);
-    const user = await datasource
-      .getRepository(User)
-      .save({ ...data, hashedPassword });
-    return user;
-  }
-
-  @Mutation(() => String)
-  async login(@Arg("data") data: UserInput): Promise<string> {
-    const user = await datasource
-      .getRepository(User)
-      .findOne({ where: { email: data.email } });
-    const hashedPassword = await hashPassword(data.password);
-
-    if (user === null) {
-      throw new ApolloError("no user");
-    } else if (!(await verifyPassword(data.password, user.hashedPassword))) {
-      throw new ApolloError("no password");
-    } else {
-      return "ok";
+    @Query(() => [User])
+    async users(): Promise<User[]> {
+        return await datasource.getRepository(User).find();
     }
-  }
+
+    @Mutation(() => User)
+    async createUser(@Arg("data") data: UserInput): Promise<User> {
+        const hashedPassword = await hashPassword(data.password);
+        const user = await datasource
+            .getRepository(User)
+            .save({...data, hashedPassword});
+        return user;
+    }
+
+    @Mutation(() => String)
+    async login(@Arg("data") data: UserInput): Promise<string> {
+        const user = await datasource
+            .getRepository(User)
+            .findOne({where: {email: data.email}});
+        const hashedPassword = await hashPassword(data.password);
+
+        if (
+            user === null ||
+            typeof user.hashedPassword !== "string" ||
+            !(await verifyPassword(data.password, user.hashedPassword))
+        )
+            throw new ApolloError("invalid credentials");
+        else {
+            return "ok";
+        }
+    }
 }
