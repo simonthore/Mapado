1e9da92756416cf68efc9adc505b88ebcede5bd3
Simon Thore <simon.thore@hotmail.fr>
2023-02-02 11:15:43 +0100

    test

--- mobile-client/screens/CityInfoScreen.tsx
+++ mobile-client/screens/CityInfoScreen.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import {View, Text, StyleSheet, Image} from "react-native";
 import {City} from "../gql/generated/schema";
 import MapView from 'react-native-maps';
+import { Route } from 'react-router-dom';
 
 interface WilderListItemProps {
     city: City;
+    
 }
 
 export default function CityInfoScreen({route}: WilderListItemProps) {
