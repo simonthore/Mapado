7fc9746cf5b759905ebcc7b2668547f3d9ebe7a4
Anna Gosme <annagosme@air-de-anna.lan>
2023-03-01 12:37:12 +0100

    fix conflicts

--- client/src/screens/Login.tsx
+++ client/src/screens/Login.tsx
@@ -1,19 +1,12 @@
 import CSS from "csstype";
 import {useState} from "react";
-<<<<<<< HEAD
 import {Navigate, Link} from "react-router-dom";
-=======
-import {Navigate} from "react-router-dom";
->>>>>>> origin
 import toast from "react-hot-toast";
 import login from "../assets/login.svg";
 import {useGetProfileQuery, useLoginMutation} from "../gql/generated/schema";
 import Card from "../components/Card";
 import {useNavigate} from "react-router";
-<<<<<<< HEAD
 import Header from "../components/Header";
-=======
->>>>>>> origin
 
 const loginPageStyles: CSS.Properties = {
     display: "flex",
@@ -77,18 +70,10 @@ const titleStyles: CSS.Properties = {
 };
 
 export default function Login() {
-<<<<<<< HEAD
   const [credentials, setCredentials] = useState({ email: "", password: "" });
   const [passwordShown, setPasswordShown] = useState(false);
-=======
-    const [credentials, setCredentials] = useState({email: "", password: ""});
-    let navigate = useNavigate();
->>>>>>> origin
-
-    const [login] = useLoginMutation();
-
-    const {data: currentUser, refetch, client} = useGetProfileQuery();
-<<<<<<< HEAD
+  const [login] = useLoginMutation();
+  const {data: currentUser, client} = useGetProfileQuery();
 
   const navigate = useNavigate();
 
@@ -175,84 +160,4 @@ export default function Login() {
       </div>
     </>
   );
-=======
-
-    function register() {
-        console.log("hello")
-        navigate("/register", {replace: false});
-    }
-
-    return (
-        <div style={loginPageStyles}>
-            {currentUser && <Navigate to="/" replace={false}/>}
-            <Card customClass={"registerCard"}>
-                <form
-                    style={loginContainerStyles}
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        login({variables: {data: credentials}})
-                            .then(() => {
-                                client.resetStore();
-                            })
-                            .catch((error) => {
-                                toast.error("Invalid credentials", {
-                                    style: {
-                                        border: "3px solid #EC5D5C",
-                                        padding: "4rem",
-                                        color: "#EC5D5C",
-                                    },
-                                    iconTheme: {
-                                        primary: "#EC5D5C",
-                                        secondary: "#FFFFFF",
-                                    },
-                                });
-                            });
-                    }}
-                >
-                    <a href="/">
-                        <h1 style={titleStyles}>MAPADO</h1>
-                    </a>
-                    {/* <img src={login} alt="" style={iconStyles} /> */}
-                    <div style={{display: "flex", flexDirection: "column", gap: "2rem", alignItems: "center"}}>
-                        <label htmlFor="email">
-                            <input
-                                style={inputStyles}
-                                type="email"
-                                id="email"
-                                name="email"
-                                placeholder="Adresse mail"
-                                value={credentials.email}
-                                onChange={(e) =>
-                                    setCredentials({...credentials, email: e.target.value})
-                                }
-                            ></input>
-                        </label>
-                        <label htmlFor="password">
-                            <input
-                                style={inputStyles}
-                                type="password"
-                                placeholder="Mot de passe"
-                                value={credentials.password}
-                                onChange={(e) =>
-                                    setCredentials({...credentials, password: e.target.value})
-                                }
-                            ></input>
-                        </label>
-                        <button type="submit" style={tertiaryButtonStyles}>
-                            Se connecter
-                        </button>
-                    </div>
-                    <p style={{color: "#173472", fontWeight: 600}}>Mot de passe oublié ? <a href={"#"}>Cliquez ici !</a>
-                    </p>
-                </form>
-                <div>
-                    <p style={{color: "white", fontWeight: 600}}>Pas encore de compte Mapado ? </p>
-                    <button style={tertiaryButtonStyles} onClick={register}>Créer un compte
-                    </button>
-                </div>
-            </Card>
-
-        </div>
-    );
->>>>>>> origin
 }
