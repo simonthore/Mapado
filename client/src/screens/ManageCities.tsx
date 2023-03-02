a33d3751ebedd7cf3ae6ddbc8f68de66c64b3553
ggors <ggorski@consoneo.com>
2023-03-02 12:51:02 +0100

    ♻️ Refacto for CSS

--- client/src/screens/ManageCities.tsx
+++ client/src/screens/ManageCities.tsx
@@ -1,4 +1,5 @@
 import { findByLabelText } from "@testing-library/react";
+import Card from "../components/Card";
 
 interface City {
     id: number;
@@ -16,28 +17,29 @@ interface Cities {
 
 export default function ManageCities({cities}: Cities) {
     return (
-        <div className={"addManageStyles"}>
-            <h2 className={"title"}>Ajouter une ville</h2>
+        <Card customClass={"registerCard"}>
+                <h2 className={"title"}>Ajouter une ville</h2>
 
-            <div className={"addCityContainer"}>
-                <input
-                    type="text"
-                    placeholder="Nom de la ville"
-                ></input>
-                <button className={"tertiaryButton"}>Ajouter</button>
-            </div>
+                <div className={"addCityContainer"}>
+                    <input
+                        type="text"
+                        placeholder="Nom de la ville"
+                    ></input>
+                    <button className={"tertiaryButton"}>Ajouter</button>
+                </div>
 
-            <div className={"manageCitiesContainer"}>
-                <h2 className={"title"}>Gérer les villes</h2>
-                {cities.map((city: City) => {
-                    return (
-                        <div className={"manageOneCityContainer"}>
-                            <p className={"cityLabel"}>{city.name}</p>
-                            <button className={"primaryButton"}>Supprimer</button>
-                        </div>
-                    );
-                })}
-            </div>
-        </div>
+                <div className={"manageCitiesContainer"}>
+                    <h2 className={"title"}>Gérer les villes</h2>
+                    {cities.map((city: City) => {
+                        return (
+                            <div className={"manageOneCityContainer"}>
+                                <p className={"cityLabel"}>{city.name}</p>
+                                <button className={"primaryButton"}>Supprimer</button>
+                            </div>
+                        );
+                    })}
+                </div>
+        </Card>
+
     );
 }
