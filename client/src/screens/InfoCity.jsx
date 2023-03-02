ddd0890a7c47669db502513a509c984e7729bb66
ggors <ggorski@consoneo.com>
2023-03-02 17:03:54 +0100

    ✨ Adding loader

--- client/src/screens/InfoCity.jsx
+++ client/src/screens/InfoCity.jsx
@@ -1,9 +1,20 @@
-import Map from  "../screens/Map"
+import Map from "../screens/Map";
+import {useParams} from "react-router-dom";
+import {useGetCityQuery} from "../gql/generated/schema";
 
 export default function InfoCity() {
+    const {cityName} = useParams();
+
+    console.log(cityName)
+
+    const {loading: loadingCities, data} = useGetCityQuery({variables: {query: cityName}})
+
+    const city = data?.city ?? [];
+
     return (
         <div>
-            <Map></Map>
+            <Map longitude={city.longitude} latitude={city.latitude}></Map>
         </div>
     )
-};
+}
+;
