c8baf5e1e7fb29207c0fc50216ad08cd2366025e
annagosme <anna.gosme@gmail.com>
2023-03-28 17:36:31 +0200

    begin E2E tests

---
+++ e2e-tests/src/home.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test";
+import db from "../../server/src/db";
+import City from "../../server/src/entity/City";
+import { clearDB, connect, disconnect } from "./dbHelpers";
+
+test.beforeAll(connect);
+test.beforeEach(clearDB);
+test.afterAll(disconnect);
+
+test("can view users in db", async ({ page }) => {
+    await db
+        .getRepository(City)
+        .insert([{ name: "Niort" }, { name: "Roubaix" }]);
+
+    await page.goto("/");
+
+    await expect(page.getByTestId("city-list")).toContainText("Niort");
+    await expect(page.getByTestId("city-list")).toContainText("Roubaix");
+});
+
+test("can add a city", async ({ page }) => {
+    await page.goto("/manage-cities");
+    await page.getByTestId("newCity").type("Brest");
+    await page.getByRole("button", { name: "Ajouter" }).click();
+    await expect(page.getByTestId("city-list")).toContainText("Brest");
+});
