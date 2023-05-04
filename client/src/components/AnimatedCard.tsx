a94eb940776e20e3a84ec6a947a9db73692147e0
Ikunat <francois.deleau@pm.me>
2023-05-04 14:39:38 +0200

    correction erreurs console structurelles

--- client/src/components/AnimatedCard.tsx
+++ client/src/components/AnimatedCard.tsx
@@ -1,24 +1,25 @@
 interface CityProps {
-    id?: number;
-    cityName?: string;
-    cityPhoto?: string | null;
+  id?: number;
+  cityName?: string;
+  cityPhoto?: string | null;
 }
 
-export default function AnimatedCard({cityName, cityPhoto}: CityProps) {
-    return (
-        <div className="card" style={{
-            backgroundImage: `url(${cityPhoto})`
-        }}>
-            <div className="card-content">
-                <h2 className="card-title">{cityName}</h2>
-                <p className="card-body">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa.
-                </p>
-                <a href="/info" className="button">
-                    Learn More
-                </a>
-            </div>
-        </div>
-    )
+export default function AnimatedCard({ cityName, cityPhoto }: CityProps) {
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundImage: `url(${cityPhoto})`,
+      }}
+    >
+      <div className="card-content">
+        <h2 className="card-title">{cityName}</h2>
+        <p className="card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio,
+          culpa.
+        </p>
+        <button>Learn More</button>
+      </div>
+    </div>
+  );
 }
-
