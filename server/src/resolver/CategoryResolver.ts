a0ad10380bcb7d87168b50bb525f2a28957668c0
ggors <ggorski@consoneo.com>
2023-05-22 12:36:12 +0200

    ✨ Basic category CRUD created

--- server/src/resolver/CategoryResolver.ts
+++ server/src/resolver/CategoryResolver.ts
@@ -1,6 +1,7 @@
-import {Arg, Mutation, Query, Resolver} from "type-graphql";
+import {Arg, Int, Mutation, Query, Resolver} from "type-graphql";
 import Category, {CategoryInput} from "../entity/Category";
 import datasource from "../db";
+import {ApolloError} from "apollo-server-errors";
 
 @Resolver(Category)
 export class CategoryResolver {
@@ -11,7 +12,29 @@ export class CategoryResolver {
 
     @Mutation(() => Category)
     async createCategory(@Arg("data") data: CategoryInput): Promise<Category> {
-      return await datasource.getRepository(Category).save(data);
+        return await datasource.getRepository(Category).save(data);
     }
+
+    @Mutation(() => Boolean)
+    async deleteCategory(@Arg("id", () => Int) id: number): Promise<boolean> {
+        const {affected} = await datasource.getRepository(Category).delete(id);
+        if (affected === 0) throw new ApolloError("Category not found", "NOT_FOUND");
+        return true;
+    }
+
+    @Mutation(() => Category)
+    async updateCategory(
+        @Arg("id", () => Int) id: number,
+        @Arg("data") { name }: CategoryInput
+    ): Promise<Category> {
+        const { affected } = await datasource
+            .getRepository(Category)
+            .update(id, { name });
+
+        if (affected === 0) throw new ApolloError("Category not found", "NOT_FOUND");
+
+        return { id, name };
+    }
+
 
 }
