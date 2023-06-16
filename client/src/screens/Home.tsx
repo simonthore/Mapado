4441858cc8966783ed8dc3c14d7716a17fa5af18
Ikunat <francois.deleau@pm.me>
2023-06-16 16:56:50 +0200

    Modification de la Home pour qu'elle soit une page d'intro

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,169 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { Link, NavLink, useSearchParams } from "react-router-dom";
-import AnimatedCard from "../components/AnimatedCard";
-import { useCitiesQuery } from "../gql/generated/schema";
-import IState from "../interfaces/IState";
+import { Link } from "react-router-dom";
+import Mapado from "../assets/images/mapado_logo.png";
 import directions from "../assets/images/directions.png";
 
 export default function Home() {
-  // gets the params from URL
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const { loading: loadingCities, data } = useCitiesQuery();
-
-  const cities = data?.cities ?? [];
-  // State to manage both URL query & cities to display
-  const [state, setState] = useState<IState>({
-    query: searchParams.get("query") ?? "",
-    list: [],
-  });
-
-  const [headerShown, setHeaderShown] = useState(true);
-
-  useEffect(() => {
-    const storedHeaderShown = localStorage.getItem("headerShown");
-    if (storedHeaderShown) {
-      // Convertir la valeur en booléen
-      setHeaderShown(storedHeaderShown === "true");
-    }
-    const handleBeforeUnload = () => {
-      // Supprimer la clé du stockage local
-      localStorage.removeItem("headerShown");
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (headerShown) {
-      document.body.style.overflow = "hidden";
-    } else document.body.style.overflow = "auto";
-    // Sauvegarder la valeur dans le stockage local
-    localStorage.setItem("headerShown", String(headerShown));
-  }, [headerShown]);
-
-  // takes in value from the search bar and returns a filtered list of the cities to display
-  //(filter improves with each letter)
-  //searchParams controls the URL (what comes after the "?")
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const results = cities.filter((city) => {
-      if (e.target.value === " ") return cities;
-      return city.name.toLowerCase().includes(e.target.value.toLowerCase());
-    });
-    setSearchParams({ query: e.target.value });
-    setState({
-      query: e.target.value,
-      list: results,
-    });
-  };
-
-  function toggleMenu() {
-    const container = document.getElementById("container"),
-      trigger = container?.querySelector("button.trigger");
-
-    // container?.classList.toggle('container--open')
-    trigger?.classList.toggle("trigger--active");
-    setHeaderShown(!headerShown);
-  }
-
-  console.log(headerShown);
-
   return (
     <>
-      <div id="container" className={!headerShown ? "container--open" : ""}>
+      <div id="container">
         <header className="intro">
-          <div
-            className="intro__image"
-            style={{ display: "flex", alignItems: "center" }}
-          >
-            <img src={directions} alt="character-with-map" />
-          </div>
-          <div className="intro__content">
-            <h1 className="intro__main__title">Locate, discover & share !</h1>
-            <h1 className="intro__title">Mapado</h1>
-            <div className="intro__subtitle">
-              <div className="codrops-links">
-                <div className="intro__description">
-                  <p>
-                    L'application qui géolocalise les centres d'intérêts à
-                    découvrir dans les villes que vous visitez.
-                  </p>
-                  <div className="demos">
-                    <Link to="/cities-list">Accueil</Link>
-                    <Link to="/admin" onClick={() => setHeaderShown(false)}>
-                      Admin
-                    </Link>
-                    <Link to="/login">Connexion</Link>
-                  </div>
-                  <div className="search-input">
-                    <form>
-                      <input
-                        value={state.query}
-                        onChange={handleChange}
-                        placeholder="Rechercher une ville..."
-                        type="search"
-                      ></input>
-                    </form>
-                  </div>
-                </div>
-                <button className="trigger" onClick={toggleMenu}>
-                  <svg
-                    width="100%"
-                    height="100%"
-                    viewBox="0 0 60 60"
-                    preserveAspectRatio="none"
-                  >
-                    <g className="icon icon--grid">
-                      <rect x="32.5" y="5.5" width="22" height="22" />
-                      <rect x="4.5" y="5.5" width="22" height="22" />
-                      <rect x="32.5" y="33.5" width="22" height="22" />
-                      <rect x="4.5" y="33.5" width="22" height="22" />
-                    </g>
-                    <g className="icon icon--cross">
-                      <line x1="4.5" y1="55.5" x2="54.953" y2="5.046" />
-                      <line x1="54.953" y1="55.5" x2="4.5" y2="5.047" />
-                    </g>
-                  </svg>
-                  <span>View content</span>
-                </button>
-              </div>
+          <Link to="/">
+            <img className="intro__logo" src={Mapado} alt="logo" />
+          </Link>
+          <div className="intro__container">
+            <div className="intro__text">
+              <h1 className="intro__main__title main-color">
+                Bienvenue sur Mapado
+              </h1>
+              <h2 className="intro__description main-color">
+                Nous avons construit une application qui vous permet d'ajouter
+                vos coins préférés à vos villes préférées. Ou futures villes
+                préférées. Même si c'est notre appli que vous allez préférer.
+              </h2>
+              <Link className="intro__go-to-app main-color" to="/cities-list">
+                Essayer Mapado
+              </Link>
             </div>
+            <img
+              className="intro__image"
+              src={directions}
+              alt="character-with-map"
+            />
           </div>
-          <button className="hero-section-trigger" onClick={toggleMenu}>
-            <div className="arrow"></div>
-          </button>
         </header>
-        <section className="items-wrap">
-          {state.query === ""
-            ? // if there is no search, display all cities
-              cities.map((city) => (
-                <NavLink
-                  className="cardLink"
-                  key={city.id}
-                  to={`/info/${city.name}`}
-                >
-                  <AnimatedCard
-                    key={city.id}
-                    cityName={city.name}
-                    cityPhoto={city.photo}
-                  />
-                </NavLink>
-              ))
-            : state.list.map((city) => (
-                // if there is a search display the cities corresponding
-                <NavLink key={city.id} to={`/info/${city.name}`}>
-                  <AnimatedCard
-                    key={city.id}
-                    cityName={city.name}
-                    cityPhoto={city.photo}
-                  />
-                </NavLink>
-              ))}
-        </section>
       </div>
     </>
   );
