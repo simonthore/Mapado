69ba7f54d54f19a9be01ce87c87001f7c26846c8
Ikunat <francois.deleau@pm.me>
2023-06-16 16:59:47 +0200

    Déplacement de la logique à la racine dans App.tsx, afin de pouvoir utiliser les fonctionnalités depuis la searchbar sur le composant CitiesList

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-import { Route, Routes } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
+import { Route, Routes, useSearchParams } from "react-router-dom";
 import CSS from "csstype";
 import Login from "./screens/Login";
 import Home from "./screens/Home";
@@ -11,12 +10,13 @@ import Register from "./screens/Register";
 import PasswordReset from "./screens/PasswordReset";
 import EmailPassword from "./screens/EmailPassword";
 import InfoCity from "./screens/InfoCity";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useLocation } from "react-router";
 import EditCity from "./screens/EditCity";
 import CitiesList from "./screens/CitiesList";
 import Admin from "./screens/Admin";
 import ManageCategories from "./screens/ManageCategories";
+import IState from "./interfaces/IState";
 
 const styles: CSS.Properties = {
   margin: 0,
@@ -24,22 +24,47 @@ const styles: CSS.Properties = {
 };
 
 function App() {
-  const { loading: loadingCities, data } = useCitiesQuery();
-  const [showHeader, setShowHeader] = useState(false);
-  const location = useLocation();
+  const { data } = useCitiesQuery();
 
-  useEffect(() => {
-    const path = window.location.pathname;
-    setShowHeader(path !== "/");
-  }, [location]);
+  const currentUrl = useLocation().pathname;
 
-  // const cities = data?.cities ?? [];
+  const cities = data?.cities ?? [];
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [state, setState] = useState<IState>({
+    query: searchParams.get("query") ?? "",
+    list: [],
+  });
+
+  // takes in value from the search bar and returns a filtered list of the cities to display
+  //(filter improves with each letter)
+  //searchParams controls the URL (what comes after the "?")
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const results = cities.filter((city) => {
+      if (e.target.value === " ") return cities;
+      return city.name.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+
+    setSearchParams({ query: e.target.value });
+
+    setState({
+      query: e.target.value,
+      list: results,
+    });
+  };
 
   return (
     <>
       {/*<Toaster position="top-center"/>*/}
       <div style={styles}>
-        {showHeader && <Header />}
+        <Header
+          currentUrl={currentUrl}
+          state={state}
+          handleChange={handleChange}
+        />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
@@ -49,7 +74,7 @@ function App() {
             path="/password/reset/:id/:token"
             element={<PasswordReset />}
           />
-          <Route path="/cities-list" element={<CitiesList />} />
+          <Route path="/cities-list" element={<CitiesList state={state} />} />
           <Route path="/info/:cityName" element={<InfoCity />} />
           <Route path="/manage-cities" element={<ManageCities />} />
           <Route path="/edit-city/:cityName" element={<EditCity />} />
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,47 +1,49 @@
-import React, {useState} from "react";
+import { useState } from "react";
 import Logout from "./Logout";
-import {Link} from "react-router-dom";
-import Mapado from "../assets/images/mapado_logo.png"
+import { Link } from "react-router-dom";
+import Mapado from "../assets/images/mapado_logo.png";
+import SearchBar from "./SearchBar";
 
-export default function Header() {
-    const [headerWithShadow, setHeaderWithShadow] = useState(false)
-    const changeNavStyle = () => {
-        if(window.scrollY >= 10){
-            setHeaderWithShadow(true)
-        }else{
-            setHeaderWithShadow(false)
-        }
+export default function Header({ currentUrl, handleChange, state }) {
+  const [headerWithShadow, setHeaderWithShadow] = useState(false);
+  const changeNavStyle = () => {
+    if (window.scrollY >= 10) {
+      setHeaderWithShadow(true);
+    } else {
+      setHeaderWithShadow(false);
     }
+  };
 
-    window.addEventListener('scroll', changeNavStyle)
+  window.addEventListener("scroll", changeNavStyle);
 
-    return (
+  return currentUrl !== "/" ? (
+    <nav
+      className={`headerStyle${headerWithShadow ? " headerWithShadow" : ""}`}
+    >
+      <Link to="/">
+        <img src={Mapado} alt="logo" />
+      </Link>
 
-        <nav className={`headerStyle${headerWithShadow ? " headerWithShadow" : ''}`}>
-            <Link to="/">
-                <img src={Mapado}/>
-            </Link>
+      <SearchBar
+        currentUrl={currentUrl}
+        state={state}
+        handleChange={handleChange}
+      />
 
-            <div className="intro__subtitle">
-                <div className="codrops-links">
-                    <div className="intro__description">
-                        <p>
-                            Locate, discover & share !
-                        </p>
-                        <div className="demos">
-                            <Link to="/cities-list">Accueil</Link>
-                            <Link to="/admin">
-                                Admin
-                            </Link>
-                            <Link to="/login">
-                                Connexion
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+      <div className="intro__subtitle">
+        <div className="codrops-links">
+          <div className="intro__description">
+            <p>Locate, discover & share !</p>
+            <div className="demos">
+              <Link to="/cities-list">Accueil</Link>
+              <Link to="/admin">Admin</Link>
+              <Link to="/login">Connexion</Link>
             </div>
-        </nav>
-
-    )
-        ;
+          </div>
+        </div>
+      </div>
+    </nav>
+  ) : (
+    <></>
+  );
 }
