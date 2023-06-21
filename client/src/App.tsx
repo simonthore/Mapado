5b3590cc0d4fe6072c7f3397049705186350191f
ggors <ggorski@consoneo.com>
2023-06-21 16:29:45 +0200

    🚧 Animation Home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useSearchParams } from "react-router-dom"
+import {Route, Routes, useSearchParams, useLocation} from "react-router-dom";
 import CSS from "csstype";
 import Login from "./screens/Login";
 import Home from "./screens/Home";
@@ -10,13 +10,13 @@ import Register from "./screens/Register";
 import PasswordReset from "./screens/PasswordReset";
 import EmailPassword from "./screens/EmailPassword";
 import InfoCity from "./screens/InfoCity";
-import { useState } from "react";
-import { useLocation } from "react-router";
+import {useState} from "react";
 import EditCity from "./screens/EditCity";
 import CitiesList from "./screens/CitiesList";
 import Admin from "./screens/Admin";
 import ManageCategories from "./screens/ManageCategories";
 import IState from "./interfaces/IState";
+import {AnimatePresence} from "framer-motion";
 
 const styles: CSS.Properties = {
     margin: 0,
@@ -24,66 +24,73 @@ const styles: CSS.Properties = {
 };
 
 function App() {
-  const { data } = useCitiesQuery();
+    const {data} = useCitiesQuery();
 
-  const currentUrl = useLocation().pathname;
+    const currentUrl = useLocation().pathname;
 
-  const cities = data?.cities ?? [];
+    const cities = data?.cities ?? [];
 
-  const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams, setSearchParams] = useSearchParams();
 
-  const [state, setState] = useState<IState>({
-    query: searchParams.get("query") ?? "",
-    list: [],
-  });
-
-  // takes in value from the search bar and returns a filtered list of the cities to display
-  //(filter improves with each letter)
-  //searchParams controls the URL (what comes after the "?")
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const results = cities.filter((city) => {
-      if (e.target.value === " ") return cities;
-      return city.name.toLowerCase().includes(e.target.value.toLowerCase());
+    const [state, setState] = useState<IState>({
+        query: searchParams.get("query") ?? "",
+        list: [],
     });
 
-    setSearchParams({ query: e.target.value });
+    // takes in value from the search bar and returns a filtered list of the cities to display
+    //(filter improves with each letter)
+    //searchParams controls the URL (what comes after the "?")
 
-    setState({
-      query: e.target.value,
-      list: results,
-    });
-  };
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const results = cities.filter((city) => {
+            if (e.target.value === " ") return cities;
+            return city.name.toLowerCase().includes(e.target.value.toLowerCase());
+        });
 
-  return (
-    <>
-      {/*<Toaster position="top-center"/>*/}
-      <div style={styles}>
-        <Header
-          currentUrl={currentUrl}
-          state={state}
-          handleChange={handleChange}
-        />
+        setSearchParams({query: e.target.value});
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/password/email" element={<EmailPassword />} />
-          <Route
-            path="/password/reset/:id/:token"
-            element={<PasswordReset />}
-          />
-          <Route path="/cities-list" element={<CitiesList state={state} />} />
-          <Route path="/info/:cityName" element={<InfoCity />} />
-          <Route path="/manage-cities" element={<ManageCities />} />
-          <Route path="/edit-city/:cityName" element={<EditCity />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/manage-categories" element={<ManageCategories />} />
-        </Routes>
-      </div>
-    </>
-  );
+        setState({
+            query: e.target.value,
+            list: results,
+        });
+    };
+
+    const location = useLocation();
+
+    return (
+        <>
+            {/*<Toaster position="top-center"/>*/}
+            <div style={styles}>
+
+                <Header
+                    currentUrl={currentUrl}
+                    state={state}
+                    handleChange={handleChange}
+                />
+                <AnimatePresence mode='wait'>
+                    <Routes location={location} key={location.pathname}>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/cities-list" element={<CitiesList state={state}/>}/>
+                    </Routes>
+                </AnimatePresence>
+                <Routes>
+
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/password/email" element={<EmailPassword/>}/>
+                    <Route
+                        path="/password/reset/:id/:token"
+                        element={<PasswordReset/>}
+                    />
+                    <Route path="/info/:cityName" element={<InfoCity/>}/>
+                    <Route path="/manage-cities" element={<ManageCities/>}/>
+                    <Route path="/edit-city/:cityName" element={<EditCity/>}/>
+                    <Route path="/admin" element={<Admin/>}/>
+                    <Route path="/manage-categories" element={<ManageCategories/>}/>
+                </Routes>
+            </div>
+        </>
+    );
 
 }
 
--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,38 +1,39 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Mapado from "../assets/images/mapado_logo.png";
 import directions from "../assets/images/directions.png";
+import {motion} from "framer-motion";
 
 export default function Home() {
-  return (
-    <>
-      <div id="container">
-        <header className="intro">
-          <Link to="/">
-            <img className="intro__logo" src={Mapado} alt="logo" />
-          </Link>
-          <div className="intro__container">
-            <div className="intro__text">
-              <h1 className="intro__main__title main-color">
-                Bienvenue sur Mapado
-              </h1>
-              <h2 className="intro__description main-color">
-                Nous avons créé une application qui vous permet d'ajouter vos
-                coins préférés à vos villes préférées. Ou futures villes
-                préférées. Même si c'est notre application que vous allez
-                préférer.
-              </h2>
-              <Link className="intro__go-to-app main-color" to="/cities-list">
-                Essayer Mapado
-              </Link>
-            </div>
-            <img
-              className="intro__image"
-              src={directions}
-              alt="character-with-map"
-            />
-          </div>
-        </header>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <motion.div exit={{opacity: 0, top: "-100vh"}}>
+                <header className="intro">
+                    <Link to="/">
+                        <img className="intro__logo" src={Mapado} alt="logo"/>
+                    </Link>
+                    <div className="intro__container">
+                        <div className="intro__text">
+                            <h1 className="intro__main__title main-color">
+                                Bienvenue sur Mapado
+                            </h1>
+                            <h2 className="intro__description main-color">
+                                Nous avons créé une application qui vous permet d'ajouter vos
+                                coins préférés à vos villes préférées. Ou futures villes
+                                préférées. Même si c'est notre application que vous allez
+                                préférer.
+                            </h2>
+                            <Link className="intro__go-to-app main-color" to="/cities-list">
+                                Essayer Mapado
+                            </Link>
+                        </div>
+                        <img
+                            className="intro__image"
+                            src={directions}
+                            alt="character-with-map"
+                        />
+                    </div>
+                </header>
+            </motion.div>
+        </>
+    );
 }
