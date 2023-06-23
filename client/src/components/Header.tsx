c8de9965667a76e01674418b6d7fe59c201d50ea
ggors <ggorski@consoneo.com>
2023-06-23 09:48:04 +0200

    ♻️ Refacto

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -27,6 +27,7 @@ export default function Header({currentUrl, handleChange, state}: HeaderProps) {
 
     window.addEventListener("scroll", changeNavStyle);
 
+    // UseEffect qui annuler l'animation
     useEffect(() => {
         if (shouldAnimate && currentUrl === "/cities-list") {
             setTimeout(() => {
@@ -55,40 +56,39 @@ export default function Header({currentUrl, handleChange, state}: HeaderProps) {
             </div>
         </nav>
 
-    return currentUrl !== "/" && currentUrl !== "/cities-list" ? ( // Si la route est '/' ou '/cities-list' on n'affiche pas le header sans animation
-        header
-    ) : currentUrl === "/cities-list" && shouldAnimate ? ( // Si la route est '/cities-list' et que shouldAnimate est à true, on affiche le header avec une animation. Par défaut shouldAnimate est à true et passe à false avec le useEffect.
-        <motion.nav
-            initial={{
-                opacity: 0,
-            }}
-            animate={{
-                opacity: 1,
-            }}
-            transition={{
-                delay: 0.5,
-                duration: 0.5,
-            }}
-            className={`headerStyle${headerWithShadow ? " headerWithShadow" : ""}`}
-        >
-            <NavLink to="/cities-list">
-                <img src={Mapado} alt="logo"/>
-            </NavLink>
+    if (!shouldAnimate) {
+        return (<motion.nav
+                initial={{
+                    opacity: 0,
+                }}
+                animate={{
+                    opacity: 1,
+                }}
+                transition={{
+                    delay: 0.5,
+                    duration: 0.5,
+                }}
+                className={`headerStyle${headerWithShadow ? " headerWithShadow" : ""}`}
+            >
+                <NavLink to="/cities-list">
+                    <img src={Mapado} alt="logo"/>
+                </NavLink>
 
-            <SearchBar currentUrl={currentUrl} state={state} handleChange={handleChange}/>
+                <SearchBar currentUrl={currentUrl} state={state} handleChange={handleChange}/>
 
-            <div className="nav__description">
-                <p>Locate, discover & share !</p>
-                <div className="demos">
-                    <NavLink to="/cities-list">Accueil</NavLink>
-                    <NavLink to="/admin">Admin</NavLink>
-                    <NavLink to="/login">Connexion</NavLink>
+                <div className="nav__description">
+                    <p>Locate, discover & share !</p>
+                    <div className="demos">
+                        <NavLink to="/cities-list">Accueil</NavLink>
+                        <NavLink to="/admin">Admin</NavLink>
+                        <NavLink to="/login">Connexion</NavLink>
+                    </div>
                 </div>
-            </div>
-        </motion.nav>
-    ) : currentUrl === "/cities-list" && !shouldAnimate ? ( // Si souldAnimate est à false, on affiche un header sans animation. C'est pour prévenir l'animation à chaque fois qu'on retourne sur la route 'cities-list'
-        header
-    ) : (
-        <></>
-    );
+            </motion.nav>
+        )
+    } else if (currentUrl === "/cities-list" && shouldAnimate) {
+        return (header)
+    } else {
+        return (<></>)
+    }
 }
